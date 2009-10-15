e7ce2cf66d898e9d46bb1cccf239d7903e8158e8
Denis <Denis@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-10-15 16:04:05 +0000

    cleanups
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/Hudson@30979 542498e7-56f8-f544-a0f2-a85913ac812a

--- src/test/java/com/versionone/hudson/AppTest.java
+++ src/test/java/com/versionone/hudson/AppTest.java
@@ -1,12 +1,12 @@
 package com.versionone.hudson;
 
-import com.gargoylesoftware.htmlunit.WebAssert;
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.model.Descriptor;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
+import hudson.tasks.BatchFile;
 import hudson.tasks.Publisher;
-import hudson.tasks.Shell;
 import hudson.util.DescribableList;
 import org.apache.commons.io.FileUtils;
 import org.jvnet.hudson.test.HudsonTestCase;
@@ -19,7 +19,7 @@ public class AppTest extends HudsonTestCase {
 
 	public void test1() throws Exception {
 		FreeStyleProject project = createFreeStyleProject();
-		project.getBuildersList().add(new Shell("echo hello"));
+		project.getBuildersList().add(new BatchFile("echo hello"));
 //		List<Action> actions = project.getActions();
 		DescribableList<Publisher, Descriptor<Publisher>> publishers = project.getPublishersList();
 		VersionOneNotifier versionOneNotifier = new VersionOneNotifier();
@@ -31,12 +31,25 @@ public class AppTest extends HudsonTestCase {
 
 		// TODO: change this to use HtmlUnit
 		String s = FileUtils.readFileToString(build.getLogFile());
-		assertTrue(s.contains("+ echo hello"));
+		assertTrue(s.contains("echo hello"));
 	}
 
 	public void test2() throws IOException, SAXException {
-		HtmlPage page = new WebClient().goTo("configure");
-		WebAssert.assertElementPresent(page, "VersionOne Integration");
-		WebAssert.assertElementPresent(page, "hudson-scm-CVSSCM");
+		final HtmlPage page = new WebClient().goTo("configure");
+		assertElementPresentByName(page, "com-versionone-hudson-VersionOneNotifier");
+	}
+
+	/**
+	 * Verifies that the specified page contains an element with the specified ID.
+	 *
+	 * @param page the page to check
+	 * @param name the expected Name of an element in the page
+	 */
+	public static void assertElementPresentByName(final HtmlPage page, final String name) {
+		try {
+			page.getHtmlElementsByName(name);
+		} catch (final ElementNotFoundException e) {
+			throw new AssertionError("The page does not contain an element with name '" + name + "'.");
+		}
 	}
 }
