c965c01eeddfe7600c6cd86523076fc59f3caa81
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-02-21 17:30:44 +0000

    we don't need requesting user keystroke after running this
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/Hudson@40636 542498e7-56f8-f544-a0f2-a85913ac812a

--- installSDK.bat
+++ installSDK.bat
@@ -4,4 +4,3 @@ call %MAVEN% install:install-file -DgroupId=com.versionone -DartifactId=apiclien
 call %MAVEN% install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.ObjectModel.jar
 call %MAVEN% install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
 call %MAVEN% install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
-pause
