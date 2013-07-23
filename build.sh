2101b212510d7a3079ad8402572619a4c5b2b916
Ian Buchanan <ian.buchanan@versionone.com>
2013-07-23 09:48:54 -0400

    document member role requirements

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+# ----- Variables -------------------------------------------------------------
+# Variables in the build.properties file will be available to Jenkins
+# build steps. Variables local to this script can be defined below.
+. ./build.properties
+
+echo Code coming soon.
